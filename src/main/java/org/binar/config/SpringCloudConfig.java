aa31732daa14c67fd309f3b1b9dc3215301d9c18
rizkymf <rizky.mochamadf@gmail.com>
2022-12-13 21:22:46 +0700

    add frontliner to gateway

--- src/main/java/org/binar/config/SpringCloudConfig.java
+++ src/main/java/org/binar/config/SpringCloudConfig.java
@@ -1,21 +1,30 @@
-//package org.binar.config;
-//
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SpringCloudConfig {
-//
-//    @Bean
-//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(r -> r.path("/first-controller/**")
-//                        .uri("http://localhost:8081/"))
-//                .route(r -> r.path("/second-controller/**")
-//                        .uri("http://localhost:8082/"))
-//                .build();
-//
-//    }
-//}
+package org.binar.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        .uri("${FIRST_SERVICE_HOST}"))
+                .route(r -> r.path("/second-service/**")
+                        .uri("${SECOND_SERVICE_HOST}"))
+                .route(r -> r.path("/frontliner/**")
+                        .uri("${FRONTLINER_HOST}"))
+//                .route(r -> r.path("/auth/**")
+//                        .uri("http://localhost:8089"))
+//                .route(r -> r.path("/binar")
+//                        .uri("https://www.binaracademy.com"))
+//                .route(r -> r.path("/9gag/**")
+//                        .uri("https://9gag.com/"))
+                .build();
+    }
+
+}
